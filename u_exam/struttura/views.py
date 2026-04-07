37131cab016dcfb3ca8c4270c6b60043aed68cbd
r.fermanelli <r.fermanelli@aktsrl.com>
2026-04-07 16:36:43 +0200

    .....

--- u_exam/struttura/views.py
+++ u_exam/struttura/views.py
@@ -2,14 +2,25 @@ from django.shortcuts import render
 
 # import dell'oggetto HttpResponse
 from django.http import HttpResponse
+# import dell'oggetto render per renderizzare i template
+from django.shortcuts import render
 
 # import degli oggetti model
-from .models import Dipartimento
+from .models import Dipartimento, CorsoDiLaurea
 
 # Create your views here.
 def dipartimento_list(request):
-    dipartimenti = []
-    for dip in Dipartimento.objects.all():
-        dipartimenti.append(dip.nome)
-    print(dipartimenti)
-    return HttpResponse("Dipartimenti: " + ", ".join(dipartimenti))
+    # dipartimenti = []
+    # for dip in Dipartimento.objects.all():
+    #     dipartimenti.append(dip.nome)
+    # print(dipartimenti)
+    # return HttpResponse("Dipartimenti: " + ", ".join(dipartimenti))
+    dipartimenti = Dipartimento.objects.all()
+    corsi_di_laurea = CorsoDiLaurea.objects.all()
+    
+    context = {
+        'dipartimenti': dipartimenti,
+        'corsi_di_laurea': corsi_di_laurea
+    }
+    # return render(request, 'struttura/dipartimento_list.html', {'dipartimenti': dipartimenti})
+    return render(request, 'dipartimento/dipartimento_list.html', context)
