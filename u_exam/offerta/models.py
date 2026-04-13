b92a91adb88298e68fefc13f94243c18c440a1f0
r.fermanelli <r.fermanelli@aktsrl.com>
2026-04-13 15:29:49 +0200

    .......

--- u_exam/offerta/models.py
+++ u_exam/offerta/models.py
@@ -67,24 +67,24 @@ class AnnoAccademico(models.Model):
         return self.nome
     
 
-    class Docenza(models.Model):
-        # segua una docstring di presentazione del modello:
-        """Modello della entità Docenza dell'area Offerta formativa"""
+class Docenza(models.Model):
+    # segua una docstring di presentazione del modello:
+    """Modello della entità Docenza dell'area Offerta formativa"""
 
-        id_docente = models.ForeignKey(Docente, on_delete=models.CASCADE, related_name="docenze")
-        id_corso = models.ForeignKey(Corso, on_delete=models.CASCADE, related_name="docenze")
-        id_anno_accademico = models.ForeignKey(AnnoAccademico, on_delete=models.CASCADE, related_name="docenze")
-        titolare = models.BooleanField(default=False)
+    id_docente = models.ForeignKey(Docente, on_delete=models.CASCADE, related_name="docenze")
+    id_corso = models.ForeignKey(Corso, on_delete=models.CASCADE, related_name="docenze")
+    id_anno_accademico = models.ForeignKey(AnnoAccademico, on_delete=models.CASCADE, related_name="docenze")
+    titolare = models.BooleanField(default=False)
     
-        # Meta è una classe interna che fornisce a Django i metadati sul modello:
-        # 1) verbose_name_plural = definisce il nome plurale del modello nell'interfaccia admin,
-        # senza questa opzione Django renderebbe il plurale in inglese scrivendo CorsoDiLaureas
-        # 2) ordering = definisce l'ordinamento di default nelle query,
-        # con ['nome'] i risultati sono determinati in ordine alfabetico per nome.
-        class Meta:
-            verbose_name_plural = 'Docenze'
+    # Meta è una classe interna che fornisce a Django i metadati sul modello:
+    # 1) verbose_name_plural = definisce il nome plurale del modello nell'interfaccia admin,
+    # senza questa opzione Django renderebbe il plurale in inglese scrivendo CorsoDiLaureas
+    # 2) ordering = definisce l'ordinamento di default nelle query,
+    # con ['nome'] i risultati sono determinati in ordine alfabetico per nome.
+    class Meta:
+        verbose_name_plural = 'Docenze'
         ordering = ['nome']
 
-        def __str__(self):
-            return self.nome
+    def __str__(self):
+        return self.nome
     
