831a42748e1677e076b8683df0cea1834614a8ee
r.fermanelli <r.fermanelli@aktsrl.com>
2026-04-22 15:21:26 +0200

    .........................

--- u_exam/offerta/admin.py
+++ u_exam/offerta/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Ssd, Corso, Docenza, AnnoAccademico
+
+admin.site.register(Ssd)
+admin.site.register(Corso)
+admin.site.register(Docenza)
+admin.site.register(AnnoAccademico)
